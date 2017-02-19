0ac065a10c2c14c4e4292ee92d961310a15e5d23
jribacruz <jribacruz@gmail.com>
2017-02-19 18:51:05 -0300

    ajuste no json

--- sk-java-api/src/main/java/br/sk/api/Project.java
+++ sk-java-api/src/main/java/br/sk/api/Project.java
@@ -1,5 +1,15 @@
 package br.sk.api;
 
+import java.nio.file.Paths;
+
+import br.sk.model.EMavenProject;
+import br.sk.proxy.EMavenProjectProxy;
+import br.sk.utils.JSON;
+
 public class Project {
 
+	public String findAllClasses(String projectDir) {
+		EMavenProject mavenProject = new EMavenProjectProxy(Paths.get(projectDir));
+		return JSON.transform(mavenProject.getClasses());
+	}
 }
