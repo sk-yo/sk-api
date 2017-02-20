43b110d67ab4c0843b79d360af86ea8ba75f3460
jribacruz <jribacruz@gmail.com>
2017-02-20 20:17:21 -0300

    ajuste na API

--- sk-java-api/src/main/java/br/sk/api/Project.java
+++ sk-java-api/src/main/java/br/sk/api/Project.java
@@ -1,8 +1,10 @@
 package br.sk.api;
 
 import java.nio.file.Paths;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import br.sk.model.EClass;
 import br.sk.model.EMavenProject;
 import br.sk.proxy.EMavenProjectProxy;
 import br.sk.utils.JSON;
@@ -19,6 +21,32 @@ public class Project {
 									.stream()
 									.anyMatch(a -> a.getName().equals(annotationName)))
 					.collect(Collectors.toList()));
+		// @formatter:on
+	}
+
+	public static String findClassesNamesByAnnotationName(String projectDir, String annotationName) {
+		EMavenProject mavenProject = new EMavenProjectProxy(Paths.get(projectDir));
+		//// @formatter:off
+		return JSON.transform(
+				mavenProject.getClasses()
+					.stream()
+					.filter(c -> c.getAnnotations()
+									.stream()
+									.anyMatch(a -> a.getName().equals(annotationName)))
+					.map(c -> c.getName())
+					.collect(Collectors.toList()));
+		// @formatter:on
+	}
+
+	public static String findClassByName(String projectDir, String className) {
+		EMavenProject mavenProject = new EMavenProjectProxy(Paths.get(projectDir));
+		//// @formatter:off
+		Optional<EClass> eClass= 
+				mavenProject.getClasses()
+					.stream()
+					.filter(c -> c.getName().equals(className))
+					.findFirst();
+		return eClass.isPresent() ? JSON.transform(eClass.get()) : null;
 		// @formatter:on
 	}
 }
--- sk-node-api/index.js
+++ sk-node-api/index.js
@@ -2,12 +2,16 @@ var java  = require('java');
 var path = require('path');
 java.classpath.push(path.resolve(__dirname, 'lib/sk-java-api-1.0.0.jar'));
 
-module.exports = class  {
-	constructor(path) {
-		this.path = path;
-	}
-	
+module.exports = {
 	findClassesByAnnotationName(name) {
-		return java.callStaticMethodSync('br.sk.api.Project','findClassesByAnnotationName', this.path, name);
+		return java.callStaticMethodSync('br.sk.api.Project','findClassesByAnnotationName', process.cwd(), name);
+	},
+	
+	findClassesNamesByAnnotationName(name) {
+		return java.callStaticMethodSync('br.sk.api.Project','findClassesNamesByAnnotationName', process.cwd(), name);
+	},
+	
+	findClassByName(name) {
+		return java.callStaticMethodSync('br.sk.api.Project','findClassByName', process.cwd(), name);
 	}
 }
