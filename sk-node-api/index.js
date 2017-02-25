8202d15fbaad02ea1b98995bbebcb5abf6bbe689
jribacruz <jribacruz@gmail.com>
2017-02-24 23:58:31 -0300

    ajuste na API

--- sk-node-api/index.js
+++ sk-node-api/index.js
@@ -4,14 +4,14 @@ java.classpath.push(path.resolve(__dirname, 'lib/sk-java-api-1.0.0.jar'));
 
 module.exports = {
 	findClassesByAnnotationName(name) {
-		return java.callStaticMethodSync('br.sk.api.Project','findClassesByAnnotationName', process.cwd(), name);
+		return JSON.parse(java.callStaticMethodSync('br.sk.api.Project','findClassesByAnnotationName', process.cwd(), name));
 	},
 	
 	findClassesNamesByAnnotationName(name) {
-		return java.callStaticMethodSync('br.sk.api.Project','findClassesNamesByAnnotationName', process.cwd(), name);
+		return JSON.parse(java.callStaticMethodSync('br.sk.api.Project','findClassesNamesByAnnotationName', process.cwd(), name));
 	},
 	
 	findClassByName(name) {
-		return java.callStaticMethodSync('br.sk.api.Project','findClassByName', process.cwd(), name);
+		return JSON.parse(java.callStaticMethodSync('br.sk.api.Project','findClassByName', process.cwd(), name));
 	}
 }
