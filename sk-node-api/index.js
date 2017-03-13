315bccdc9f0ab058ffc83b46746c4eedb6eaf3d1
jribacruz <jribacruz@gmail.com>
2017-03-13 10:48:23 -0300

    ajuste a API

--- sk-node-api/index.js
+++ sk-node-api/index.js
@@ -1,27 +1,20 @@
 var java  = require('java');
 var path = require('path');
 const _ = require('lodash');
-java.classpath.push(path.resolve(__dirname, 'lib/sk-java-api-1.0.9.jar'));
+java.classpath.push(path.resolve(__dirname, 'lib/sk-java-api-1.1.0.jar'));
 
 module.exports = {
-	findClassesByAnnotationName(name) {
-		return JSON.parse(java.callStaticMethodSync('br.sk.api.Project','findClassesByAnnotationName', process.cwd(), name));
-	},
-	
-	findClassesNamesByAnnotationName(name) {
-		return JSON.parse(java.callStaticMethodSync('br.sk.api.Project','findClassesNamesByAnnotationName', process.cwd(), name));
-	},
-	
-	findClassByName(name) {
-		return JSON.parse(java.callStaticMethodSync('br.sk.api.Project','findClassByName', process.cwd(), name));
-	},
-	
-	findAttributeWithAnnotationName(attrs, annotationName) {
-		return _.find(attrs, function(attr) { 
-            return _.find(attr.annotations, function(ann) {
-                 return ann.name === annotationName;
-            }); 
-        });
-	},
+	/**
+	 * Retorna a entidade
+	 * 
+	 * @param {*} entityName 
+	 */
+	findByName(entityName) {
+		return JSON.parse(java.callStaticMethodSync('br.sk.api.Entities', 'findByName',  process.cwd(), name));
+	}
+
+	findAll() {
+
+	}
 
 }
